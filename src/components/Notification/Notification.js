045e1c78e24e0d0842c3287543824738387f6eb8
Nadia Lut <3197344@gmail.com>
2022-05-16 11:22:43 +0300

    Notification.propTypes

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,7 +1,11 @@
 import s from './Notification.module.css';
+import PropTypes from 'prop-types'
 
 const Notification = ({ message }) => {
     return <div className={s.notification}>{message}</div>;
 };
 
+Notification.propTypes = {
+    message: PropTypes.string.isRequired,
+}
 export default Notification;
